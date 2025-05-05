cfb74258f7adb24ba4ebbb1c1a2376fa0bff4830
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-05-05 10:33:24 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { Routes, Route } from "react-router";
+import {
+  CREDITS_GAME_FINAL_NO_OPTIONS,
+  CREDITS_GAME_FINAL_PAYMENT_DATE,
+  CREDITS_GAME_FINAL_PAYMENT_SCHEDULE,
+  CREDITS_GAME_FINAL_PROTECTION,
+  CREDITS_GAME_FINAL_REDUCED_RATE,
+  CREDITS_GAME_INTERACTION_ATTEMPTS,
+  CREDITS_GAME_INTERACTION_INFINITE,
+  CREDITS_GAME_LANDING_INFINITE,
+  CREDITS_GAME_STUB,
+} from "./constants/credits-game";
+import { CreditsGameLanding } from "./components/credits-game-landing";
+import { CreditsGame } from "./components/credits-game";
+import { CreditsGameFinal } from "./components/credits-game-final";
+import { CreditsGameStub } from "./components/credits-game-stub";
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route
+        path={CREDITS_GAME_LANDING_INFINITE}
+        element={<CreditsGameLanding variant="infinite" />}
+      />
+      <Route
+        path={CREDITS_GAME_INTERACTION_ATTEMPTS}
+        element={<CreditsGame variant="attempts" />}
+      />
+      <Route
+        path={CREDITS_GAME_INTERACTION_INFINITE}
+        element={<CreditsGame variant="infinite" />}
+      />
+      <Route
+        path={CREDITS_GAME_FINAL_NO_OPTIONS}
+        element={<CreditsGameFinal variant="noOptions" />}
+      />
+      <Route
+        path={CREDITS_GAME_FINAL_REDUCED_RATE}
+        element={<CreditsGameFinal variant="reducedRate" />}
+      />
+      <Route
+        path={CREDITS_GAME_FINAL_PAYMENT_DATE}
+        element={<CreditsGameFinal variant="paymentDate" />}
+      />
+      <Route
+        path={CREDITS_GAME_FINAL_PROTECTION}
+        element={<CreditsGameFinal variant="protection" />}
+      />
+      <Route
+        path={CREDITS_GAME_FINAL_PAYMENT_SCHEDULE}
+        element={<CreditsGameFinal variant="paymentSchedule" />}
+      />
+      <Route path={CREDITS_GAME_STUB} element={<CreditsGameStub />} />
+    </Routes>
+  );
+};
---
+++ src/components/credits-game-stub/index.tsx
@@ -0,0 +1,61 @@
+import smile from "../../assets/credits-game/smile.png";
+import { Typography } from "@alfalab/core-components/typography";
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+import styles from "./index.module.css";
+import { Gap } from "@alfalab/core-components/gap";
+import { BONUS_CONFIG } from "../../constants/credits-game";
+import { LS, LSKeys } from "../../ls";
+
+export const CreditsGameStub = () => {
+  const getOneMonthSub = () => {
+    const activeBonus = BONUS_CONFIG.find(
+      ({ bonusVariant }) =>
+        bonusVariant ===
+        LS.getItem(LSKeys.CREDITS_GAME_BONUS_VARIANT, "noOptions"),
+    );
+
+    window.gtag("event", "3678_podpiska_click", {
+      variant_name: activeBonus
+        ? activeBonus.variantName
+        : "ghk_3678_2_nothing",
+      id: LS.getItem(LSKeys.USER_UUID, ""),
+    });
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <img
+          alt="Картинка ракеты"
+          src={smile}
+          width={48}
+          className={styles.image}
+        />
+        <Typography.TitleResponsive
+          font="system"
+          tag="h1"
+          view="small"
+          defaultMargins={false}
+          weight="bold"
+          style={{ marginTop: "24px", marginBottom: "12px", fontWeight: 500 }}
+        >
+          Спасибо за участие
+        </Typography.TitleResponsive>
+        <Typography.Text tag="p" view="primary-medium">
+          Сейчас дорабатываем игру — забрать награды за неё пока не получится.
+          За участие дарим 1 месяц подписки для новых пользователей Альфа-Смарт
+        </Typography.Text>
+      </div>
+
+      <div className={styles.button}>
+        <ButtonMobile block view="primary" href="" onClick={getOneMonthSub}>
+          1 месяц подписки бесплатно
+        </ButtonMobile>
+        <Gap size={16} />
+        <ButtonMobile block view="secondary" href="" onClick={() => {}}>
+          На главный
+        </ButtonMobile>
+      </div>
+    </>
+  );
+};
