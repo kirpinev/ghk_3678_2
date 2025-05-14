655d6a2d67d3820b3a0d7aa861761c60314ec3b3
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-05-14 17:29:15 +0300

    fix

--- src/components/credits-game-stub/index.tsx
+++ src/components/credits-game-stub/index.tsx
@@ -52,7 +52,7 @@ export const CreditsGameStub = () => {
           1 месяц подписки бесплатно
         </ButtonMobile>
         <Gap size={16} />
-        <ButtonMobile block view="secondary" href="alfabank://">
+        <ButtonMobile block view="secondary" href="alfabank://dashboard">
           На главный
         </ButtonMobile>
       </div>
